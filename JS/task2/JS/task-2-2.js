96b171d455669e6aff8ca41564b3472e2ce76504
zhuchunyu1995 <920683808@qq.com>
2019-12-26 16:46:40 +0800

    1

--- JS/task2/JS/task-2-2.js
+++ JS/task2/JS/task-2-2.js
@@ -1,106 +1,84 @@
+function group(value) {           //关联滑块和玩家数量，赋值并分配幽灵平民人数
+    var correlation = [player, sliding];
+    for (var i = 0; i < correlation.length; i++) {
+        correlation[i].value = value;
+    }
+    specterNum = Math.round(value * 0.292);   //取随机值，四舍五入，幽灵人数
+    civilianNum = value - specterNum;           // 平民人数
+    $(specter).val(specterNum);      //赋值给幽灵
+    $(civilian).val(civilianNum);     //赋值给平民
 
-function tr(value) { // 关联玩家人数和滑块
-    var a = [player, sliding];
-    for (var i = 0; i < a.length; i++) {
-        a[i].value = value;
+    var distribution = [];         //空数组，接收幽灵和平民随机分配的数值
+    for (var ghost = 0; ghost < specter.value; ghost++) {
+        distribution.push("幽灵");
+    }
+    for (var citizen = 0; citizen < civilian.value; citizen++) {
+        distribution.push("水民");
     }
 
-    
-    var kk=[];    //随机生成赋值的数组4-18
-        for(i=0;i<value;i++) {
-         kk[i]=i;  
+    for (var i = 0; i < distribution.length; i++) {
+        var r = Math.floor(Math.random() * distribution.length);
+        var t = distribution[i];
+        distribution[i] = distribution[r];
+        distribution[r] = t;
     }
 
-
-
-
-        kk.sort(function(){
-          var  y=Math.round(i * 0.292);
-          var  x=i-y;
-            $(specter).val(y);
-            $(civilian).val(x);
-            return Math.random() - 0.5;
-
-        });
-
-        console.log(kk)
-
-        var identity=[];
-        
-        for (var k = 0; k < specter.value; k++) {
-            identity.push("幽灵");
-            
-        }
-         for (var c = 0; c < civilian.value; c++) {
-            identity.push("水民");
-    
-        }
-        identity.sort(function(){
-            return Math.random() - 0.5;
-        });
-        console.log(identity);
-
-        return identity;
-
+    console.log(distribution)
 }
 
 
+subtract.onclick = function () {    //减号 大于4的时候可以用
+    var slide = sliding;
+    var minus = slide.value;
+    if (minus > 4) {
+        minus--;
+    }
+    specterNum = Math.round(minus * 0.292);
+    civilianNum = minus - specterNum;
+    $(specter).val(specterNum);
+    $(civilian).val(civilianNum);
 
-subtract.onclick = function () { //减号
-    var t = sliding;
-    var s = t.value;
-    if (s > 4) {
-        s--;
+
+    $(player).val(minus);
+    $(sliding).val(minus);
+}
+
+add.onclick = function () { //加号  小于18可以用
+    var slide = sliding;
+    var minus = slide.value;
+    if (minus < 18) {
+        minus++;
     }
 
-    
-    y=Math.round(s * 0.292);
-    x=s-y;
-    $(specter).val(y);
-    $(civilian).val(x);
+    specterNum = Math.round(minus * 0.292);
+    civilianNum = minus - specterNum;
 
-    $(player).val(s);
-    $(sliding).val(s);
-   
+    $(specter).val(specterNum);
+    $(civilian).val(civilianNum);
+
+    $(player).val(minus);
+    $(sliding).val(minus);
 }
 
-add.onclick = function () { //加号
-    var t = sliding;
-    var s = t.value;
-    if (s < 18) {
-        s++;
+function deal() {     //判断人数
+    var player = document.getElementById('player');
+    var judge = player.value;
+    if (judge < 4 || judge > 18) {
+        alert('请输入正确的玩家数量')
+        $("#player").val("4");
     }
-    
-    y=Math.round(s * 0.292);
-    x=s-y;
-
-    $(specter).val(y);
-    $(civilian).val(x);
-
-    $(player).val(s);
-    $(sliding).val(s);
-}
-
-function acquire() {
-    var pko=document.getElementById('player');
-    var s=pko.value;
-   if(s<4 || s>18) {
-       alert('请输入正确的玩家数量')
-   }
 
 }
 
+document.onkeydown = function (event) {     //键盘事件
+    var e = event || window.event || arguments.callee.caller.arguments[0];
+    if (e && e.keyCode == 27) { // 按 Esc 
+        //要做的事情
+    }
 
-
-document.onkeydown=function(event){
-               var e = event || window.event || arguments.callee.caller.arguments[0];
-               if(e && e.keyCode==27){ // 按 Esc 
-                    //要做的事情
-              }
-                     
-                if(e && e.keyCode==13){ // enter 键
-                    acquire()
-               }
-           }
+    if (e && e.keyCode == 13) { // enter 键
+        deal()
+    }
+}
 
 
--- JS/task3/JS/task-2-1.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-    $(".an").click(function() {
-        window.location.href="https://zhuchunyu1995.github.io/xzy/JS/task2/task-2-1.html";
-    });
-});
